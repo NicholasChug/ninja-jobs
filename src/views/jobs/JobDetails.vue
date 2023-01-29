98a416e3183aa6ea0cddd7600808ebae186c7c92
Nicholas Chug <nicholas.chug@gmail.com>
2023-01-29 14:44:48 -0500

    added in a mock fetch request to showcase how to download data from a database and render with vue

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <h2>{{ job.details }}</h2>
+    <p>The job id is {{ id }}</p>
+  </div>
+  <div v-else>
+    <p>Loading details...</p>
+  </div>
 </template>
 
 <script>
@@ -8,11 +14,17 @@ export default {
     props: [
         'id'
     ],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // }
+    data() {
+      return {
+        job: null
+      }
+    },
+    mounted() {
+      fetch('http://localhost:3000/jobs/' + this.id)
+        .then((res) => res.json())
+        .then((data) => this.jobs = data)
+        .catch((err) => console.log(err.message))
+    }
 }
 </script>
 
